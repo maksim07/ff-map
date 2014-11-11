40e9d7b84c7ab47ba8d4ec173b7b6f8a0ffc323a
Maxim Osipov <maksim07@gmail.com>
2014-11-12 01:12:21 +0300

    first implementation of map. just tree methods: put, get and size

--- src/main/java/org/fstruct/fmap/FMap.java
+++ src/main/java/org/fstruct/fmap/FMap.java
@@ -10,50 +10,18 @@ public class FMap<K, V> implements Map<K,V> {
     /**
      * This factor is used to increase capacity of the collection in case of it is full
      */
-    public static final double LOAD_FACTOR = 0.25;
+    public static final double LOAD_FACTOR = 1.25;
 
     /**
      * The part of the capacity additionally allocated for collision zone
      */
-    public static final double COLLIZION_AREA_FACTOR = 0.1;
+    public static final double COLLISION_AREA_FACTOR = 0.1;
 
-    /**
-     * Current capacity
-     */
     private int capacity;
 
-    /**
-     * Count of elements in the collection
-     */
-    private int size;
+    private LinkedTable main;
 
-    /**
-     * Size of the collision area
-     */
-    private int collisionAreaSize;
-
-    /**
-     * Cursor in the collision area
-     */
-    private int tail;
-
-    /**
-     * Each element i of the array either:
-     * - zero: means that there is no key in the map with hash % capacity == i
-     * - positive integer: index in the collision area of next object with such hashcode % capacity
-     * - negative integer means that there is some value, but no next value
-     */
-    private int[] index;
-
-    /**
-     * Keys array
-     */
-    private Object[] keys;
-
-    /**
-     * Values array
-     */
-    private Object[] values;
+    private LinkedTable tail;
 
 
     public FMap(int capacity) {
@@ -61,21 +29,18 @@ public class FMap<K, V> implements Map<K,V> {
             throw new IllegalArgumentException("Capacity has to be greater than zero");
 
         this.capacity = capacity;
-        this.collisionAreaSize = (int)(capacity * COLLIZION_AREA_FACTOR);
-        int arraySize = this.capacity + this.collisionAreaSize;
-        index = new int[arraySize];
-        keys = new Object[arraySize];
-        values = new Object[arraySize];
+        main = new LinkedTable(capacity);
+        tail = new LinkedTable((int)(capacity * COLLISION_AREA_FACTOR));
     }
 
     @Override
     public int size() {
-        return size;
+        return main.size + tail.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return size() == 0;
     }
 
     @Override
@@ -85,7 +50,11 @@ public class FMap<K, V> implements Map<K,V> {
 
     @Override
     public boolean containsValue(Object value) {
-        for (Object v : values)
+        for (Object v : main.values)
+            if (v.equals(value))
+                return true;
+
+        for (Object v : tail.values)
             if (v.equals(value))
                 return true;
 
@@ -94,70 +63,77 @@ public class FMap<K, V> implements Map<K,V> {
 
     @Override
     public V get(Object key) {
-        int i = indexByHash(key);
-        int indexValue = this.index[i];
+        int hash = hashcode(key);
+        int basket = hashIndex(hash, capacity);
 
-        while (indexValue != 0) {
+        boolean basketExists = main.hasRow(basket);
 
-            if (keys[i] != null && keys[i].equals(key))
-                return (V) values[i];
-
-            if (indexValue > 0) {
-                indexValue = index[indexValue];
-                i = indexValue;
-            }
-            else
-                break;
+        // if it is the first key with such hash
+        if (!basketExists) {
+            return null;
         }
+        // if such hash is already in the map
+        else {
+            int row = basket;
+            LinkedTable table = main;
 
-        return null;
+            do {
+                if (equals(table.keys[row], key)) {
+                    return (V) table.values[row];
+                }
+
+                if (!table.hasNextRow(row))
+                    return null;
+
+                row = table.getNextRow(row);
+                table = tail;
+
+            } while(true);
+        }
     }
+
 
     @Override
     public V put(K key, V value) {
-        if (size >= keys.length)
-            throw new RuntimeException("Resizing of the hashmap is not supported yet");
+        int hash = hashcode(key);
+        int basket = hashIndex(hash, capacity);
 
-        // get next free index
-        int i = indexByHash(key);
+        boolean basketExists = main.hasRow(basket);
 
-        // if it is zero
-        int indexValue = this.index[i];
-
-        while (true) {
-            // if there is no value with such hashcode
-            if (indexValue == 0) {
-                this.keys[i] = key;
-                this.values[i] = value;
-                this.index[i] = -1;
-                size++;
-                break;
-            }
-            // if there is some value with this hashcode
-            else {
-                if (keys[i].equals(key)) {
-                    values[i] = value;
-                    break;
-                }
-                else if (indexValue < 0) {
-                    index[i] = tail + capacity;
-                    index[tail] = -1;
-                    keys[tail] = key;
-                    values[tail] = value;
-                    tail++;
-                    size++;
-                    break;
-                }
-                else {
-                    indexValue = index[indexValue];
-                    i = indexValue;
-                }
-            }
+        // if it is the first key with such hash
+        if (!basketExists) {
+            main.setRowAndIncreaseSize(basket, hash, key, value);
         }
+        // if such hash is already in the map
+        else {
+            int row = basket;
+            int last = -1;
+            LinkedTable table = main;
 
-        // ned resize
-        if (tail + capacity >= keys.length) {
-            resize();
+            do {
+                if (equals(table.keys[row], key)) {
+                    table.values[row] = value;
+                    return value;
+                }
+
+                if (!table.hasNextRow(row))
+                    break;
+
+                last = row;
+                row = table.getNextRow(row);
+                table = tail;
+
+            } while(true);
+
+            tail = tail.increaseIfNeeded(LOAD_FACTOR);
+
+            if (table == main) {
+                int link = tail.addRow(hash, key, value);
+                main.setLinkTo(row, link);
+            }
+            else {
+                tail.addNextRow(row, hash, key, value);
+            }
         }
 
         return value;
@@ -180,12 +156,12 @@ public class FMap<K, V> implements Map<K,V> {
 
     @Override
     public Set<K> keySet() {
-        return new HashSet<K>(Arrays.<K>asList((K[]) keys));
+        return null;
     }
 
     @Override
     public Collection<V> values() {
-        return Arrays.<V>asList((V[]) values);
+        return null;
     }
 
     @Override
@@ -193,26 +169,133 @@ public class FMap<K, V> implements Map<K,V> {
         throw new RuntimeException("Method is not supported");
     }
 
-    /**
-     * Returns index of the key according to the current capacity
-     *
-     * @param key key
-     * @return index in the index array or -1 if there is not next index
-     */
-    private int indexByHash(Object key) {
-        int hash = key.hashCode();
-        hash = hash < 0 ? -hash : hash;
 
-        return hash % capacity;
+    private int hashcode(Object key) {
+        if (key == null) return 0;
+
+        int h = key.hashCode();
+        h ^= (h >>> 20) ^ (h >>> 12);
+        return h ^ (h >>> 7) ^ (h >>> 4);
     }
 
-    private int nextIndex(int index) {
-        return (int)(this.index[index]);
+    private int hashIndex(int h, int length) {
+        return (h > 0 ? 1 : -1) * (h % length);//h & (length - 1);
     }
 
-    private void resize() {
-
-        System.out.println("Resizing");
+    private boolean equals(Object key1, Object key2) {
+        if (key1 == null) return key2 == null;
+        else return key1.equals(key2);
     }
 
+    private static class LinkedTable {
+        private int size;
+        private int capacity;
+        private int[] tails;
+        private final int[] hashes;
+        private final Object[] keys;
+        private final Object[] values;
+
+        LinkedTable(int capacity) {
+            this.capacity = capacity;
+            this.size = 0;
+            this.hashes = new int[capacity];
+            this.tails = new int[capacity];
+            this.keys = new Object[capacity];
+            this.values = new Object[capacity];
+        }
+
+        private LinkedTable(int size, int capacity, int[] tails, int[] hashes, Object[] keys, Object[] values) {
+            this.size = size;
+            this.capacity = capacity;
+            this.hashes = hashes;
+            this.tails = tails;
+            this.keys = keys;
+            this.values = values;
+        }
+
+        /**
+         * If the row is not null
+         *
+         * @param row row index
+         * @return true if it is not null
+         */
+        private boolean hasRow(int row) {
+            return (tails[row] >>> 31) == 1;
+        }
+
+        /**
+         * Returns true if next to <code>row</code> is not null
+         *
+         * @param row row index
+         * @return true or false according to existence of next row
+         */
+        private boolean hasNextRow(int row) {
+            return hasRow(row) && ((tails[row] & 0x7FFFFFFF) ^ 0x7FFFFFFF) != 0;
+        }
+
+        private int getNextRow(int row) {
+            return tails[row] & 0x7FFFFFFF;
+        }
+
+        /**
+         * Adds row to the table
+         *
+         * @param hash hash value of the key
+         * @param key key field
+         * @param value value field
+         * @return rownum of inserted tuple
+         */
+        private int addRow(int hash, Object key, Object value) {
+            tails[size] = ~0;
+            hashes[size] = hash;
+            keys[size] = key;
+            values[size] = value;
+            return size++;
+        }
+
+        private void setRowAndIncreaseSize(int row, int hash, Object key, Object value) {
+            tails[row] = ~0;
+            hashes[row] = hash;
+            keys[row] = key;
+            values[row] = value;
+            size ++;
+        }
+
+        private void setLinkTo(int row, int link) {
+            tails[row] = (1 << 31) | link;
+        }
+
+        /**
+         * Inserts row to the table which is next row for <code>current</code> row.
+         * Method does not check if there is already some value has been written to current row.
+         *
+         * @param current current row
+         * @param hash hash to save in the row
+         * @param key key to save in the row
+         * @param value value to save in the row
+         * @return rownum
+         */
+        private int addNextRow(int current, int hash, Object key, Object value) {
+            tails[current] = (1 << 31) | size;
+            tails[size] = ~0;
+            hashes[size] = hash;
+            keys[size] = key;
+            values[size] = value;
+            return size++;
+        }
+
+        private LinkedTable increaseIfNeeded(double factor) {
+            if (size < capacity)
+                return this;
+
+            int newCapacity = (int)(capacity * factor);
+
+            int[] newTails = Arrays.copyOf(tails, newCapacity);
+            int[] newHashes = Arrays.copyOf(hashes, newCapacity);
+            Object[] newKeys = Arrays.copyOf(keys, newCapacity);
+            Object[] newValues = Arrays.copyOf(values, newCapacity);
+
+            return new LinkedTable(size, newCapacity, newTails, newHashes, newKeys, newValues);
+        }
+    }
 }
--- src/test/java/org/fstruct/fmap/FMapTest.java
+++ src/test/java/org/fstruct/fmap/FMapTest.java
@@ -4,29 +4,32 @@ import com.carrotsearch.sizeof.RamUsageEstimator;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
 public class FMapTest {
 
+    public static final int COUNT_OF_ITEMS = 2500000;
+
     @org.junit.Test
     public void testPut() throws Exception {
-        Map<Integer, Integer> map1 = new FMap<Integer, Integer>(2500000);
-        Map<Integer, Integer> map2 = new HashMap<Integer, Integer>();
+        Map<String, Integer> map1 = new FMap<>(700001);
+        Map<String, Integer> map2 = new HashMap<>();
 
-        for (int i = 0; i < 2500000; i ++) {
-            map1.put(i , i);
-            map2.put(i, i);
+        Random random = new Random();
+        for (int i = 0; i < COUNT_OF_ITEMS; i ++) {
+            String key = new UUID(random.nextLong(), random.nextLong()).toString();
+            map1.put(key, i);
+            map2.put(key, i);
             assertEquals(map1.size(), map2.size());
+            assertEquals("For index " + key, map1.get(key), map2.get(key));
         }
 
         long size1 = RamUsageEstimator.sizeOf(map1);
         long size2 = RamUsageEstimator.sizeOf(map2);
         System.out.println("Sizes are " + size1 + " vs " + size2 + " and the ratio is " + ((double) size1 / size2) +
-                " or " + (size2 - size1) / 2500000 + " per element; or " + (size2 - size1) / (1024 * 1024) + " mb");
-
-        for (int i = 0; i < 2500000; i ++) {
-            assertEquals(map1.get(i / 2), map2.get(i / 2));
-        }
+                " or " + (size2 - size1) / COUNT_OF_ITEMS + " bytes per element; or " + (size2 - size1) / (1024 * 1024) + " mb");
     }
 }
