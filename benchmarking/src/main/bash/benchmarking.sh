428325368a314c0a3d00395b16b9df1e87b0501f
Maxim Osipov <maksim07@gmail.com>
2014-11-27 15:37:33 -0500

    stop filtering performance benchmark output

--- benchmarking/src/main/bash/benchmarking.sh
+++ benchmarking/src/main/bash/benchmarking.sh
@@ -4,7 +4,7 @@ echo "Benchmarking..."
 sdir=`dirname $0`
 
 echo "Performance test"
-java -jar $sdir/benchmarking.jar performance | awk '/Run complete/,/xxx/ {print $0}' > performance.log
+java -jar $sdir/benchmarking.jar performance > performance.log
 
 echo "Memory test"
 java -jar $sdir/benchmarking.jar memory > memory.log
